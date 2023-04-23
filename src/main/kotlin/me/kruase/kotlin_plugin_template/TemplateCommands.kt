5d65ed62bd3f0b7445bc0b5858f1c73f47c99021
KruASe76 <kruanse@ya.ru>
2023-04-24 01:28:47 +0300

    hasPluginPermission

--- src/main/kotlin/me/kruase/kotlin_plugin_template/TemplateCommands.kt
+++ src/main/kotlin/me/kruase/kotlin_plugin_template/TemplateCommands.kt
@@ -6,6 +6,7 @@ import org.bukkit.command.Command
 import org.bukkit.entity.Player
 import org.bukkit.ChatColor
 import me.kruase.kotlin_plugin_template.commands.*
+import me.kruase.kotlin_plugin_template.util.hasPluginPermission
 
 
 class TemplateCommands : TabExecutor {
@@ -18,11 +19,11 @@ class TemplateCommands : TabExecutor {
         val fullArgs = args.dropLast(1)
         return when (fullArgs.getOrNull(0)) {
             null -> Template.userConfig.messages.help.keys
-                .filter { sender.hasPermission("template.${it.replace("-", ".")}") } - "header"
+                .filter { sender.hasPluginPermission(it.replace("-", ".")) } - "header"
             "help" -> when {
-                sender.hasPermission("template.help") -> when (fullArgs.getOrNull(1)) {
+                sender.hasPluginPermission("help") -> when (fullArgs.getOrNull(1)) {
                     null -> Template.userConfig.messages.help.keys
-                        .filter { sender.hasPermission("template.${it.replace("-", ".")}") } - "header"
+                        .filter { sender.hasPluginPermission(it.replace("-", ".")) } - "header"
                     else -> listOf()
                 }
                 else -> listOf()
@@ -37,7 +38,7 @@ class TemplateCommands : TabExecutor {
                 null -> help(sender, arrayOf())
                 "help" -> help(sender, args.drop(1).toTypedArray())
                 "reload" -> {
-                    if (!sender.hasPermission("template.reload")) throw UnsupportedOperationException()
+                    if (!sender.hasPluginPermission("reload")) throw UnsupportedOperationException()
                     Template.userConfig = Template.instance.getUserConfig()
                 }
             }
--- src/main/kotlin/me/kruase/kotlin_plugin_template/commands/help.kt
+++ src/main/kotlin/me/kruase/kotlin_plugin_template/commands/help.kt
@@ -2,16 +2,17 @@ package me.kruase.kotlin_plugin_template.commands
 
 import org.bukkit.command.CommandSender
 import me.kruase.kotlin_plugin_template.Template.Companion.userConfig
+import me.kruase.kotlin_plugin_template.util.hasPluginPermission
 
 
 fun help(sender: CommandSender, args: Array<out String>) {
-    if (!sender.hasPermission("template.help")) throw UnsupportedOperationException()
+    if (!sender.hasPluginPermission("help")) throw UnsupportedOperationException()
 
     if (args.size > 1) throw IllegalArgumentException()
 
     when (args.getOrNull(0)) {
         null -> userConfig.messages.help.keys
-            .filter { sender.hasPermission("template.${it.replace("-", ".")}") || it == "header"}
+            .filter { sender.hasPluginPermission(it.replace("-", ".")) || it == "header"}
             .forEach { sender.sendMessage(arrayOf(userConfig.messages.help[it])) }
         in userConfig.messages.help.keys - "header" -> sender.sendMessage(arrayOf(userConfig.messages.help[args[0]]))
         else -> throw IllegalArgumentException()
